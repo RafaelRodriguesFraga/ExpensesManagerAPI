94b09a339d676bcc769496e76189accf0627f5f1
Weverton Pires <30535993+magpires@users.noreply.github.com>
2024-03-26 17:21:48 -0300

    Update UserRepository

--- ExpensesManager.Domain/Repositories/IUserReadRepository.cs
+++ ExpensesManager.Domain/Repositories/IUserReadRepository.cs
@@ -6,6 +6,5 @@ namespace ExpensesManager.Domain.Repositories
     public interface IUserReadRepository : IBaseReadRepository<User>
     {
         Task<User> GetByEmaillAsync(string email);
-        User GetByEmaill(string email);
     }
 }
--- ExpensesManager.Infra/Repositories/UserReadRepository.cs
+++ ExpensesManager.Infra/Repositories/UserReadRepository.cs
@@ -8,46 +8,16 @@ namespace ExpensesManager.Infra.Repositories
 {
     public class UserReadRepository : BaseReadRepository<User>, IUserReadRepository
     {
-        private readonly BaseContext _context;
         public UserReadRepository(BaseContext context) : base(context)
         {
-            _context = context;
-        }
-
-        public IEnumerable<User> GetAll()
-        {
-            return _context.Set<User>().OrderBy(user => user.Email).ToList();
-        }
-
-        public async Task<IEnumerable<User>> GetAllAsync()
-        {
-            return await _context.Set<User>().OrderBy(user => user.Email).ToListAsync();
-        }
-
-        public User GetByEmaill(string email)
-        {
-            return  _context.Set<User>()
-                .AsNoTracking()
-                .Where(user => user.Email.Trim() == email.Trim())
-                .FirstOrDefault();
         }
 
         public async Task<User> GetByEmaillAsync(string email)
         {
-            return await _context.Set<User>()
+            return await Set
                 .AsNoTracking()
                 .Where(user => user.Email.Trim() == email.Trim())
                 .FirstOrDefaultAsync();
-        }
-
-        public User GetById(Guid guid)
-        {
-            return _context.Set<User>().Find(guid);
-        }
-
-        public async Task<User> GetByIdAsync(Guid guid)
-        {
-            return await _context.Set<User>().FindAsync(guid);
         }
     }
 }
