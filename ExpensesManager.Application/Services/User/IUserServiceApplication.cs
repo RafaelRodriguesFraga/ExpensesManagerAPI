7fb204bbd2cb7d143e63aac3dad987ffc5b2b275
Rafael Rodrigues Fraga <ti.rafaelfraga@gmail.com>
2024-04-15 00:13:02 -0300

    Add Authentication and Authorization

--- ExpensesManager.Application/Services/User/IUserServiceApplication.cs
+++ ExpensesManager.Application/Services/User/IUserServiceApplication.cs
@@ -1,12 +1,15 @@
 using DotnetBaseKit.Components.Application.Base;
 using expensesManager.Application.Services;
+using ExpensesManager.Application.Login;
 using ExpensesManager.Application.ViewModels;
+using ExpensesManager.Application.ViewModels.User;
 using ExpensesManager.Domain.DTOs;
 
 namespace ExpensesManager.Application.Services.User
 {
     public interface IUserServiceApplication : IBaseServiceApplication
     {
+        Task<UserViewModel> AuthenticateAsync(LoginRequestViewModel loginViewModel);
         Task CreateAsync(UserRequestDto userRequestDto);
     }
 }
