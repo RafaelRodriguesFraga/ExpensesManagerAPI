7a89d2143c6edc5c63ae111891b2c48f6cac9049
Rafael Rodrigues Fraga <ti.rafaelfraga@gmail.com>
2024-04-14 23:45:14 -0300

    Add extension methods from services and repositories

---
+++ ExpensesManager.Infra.IoC/RepositoryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ExpensesManager.Application.Services.Person;
+using ExpensesManager.Domain.Repositories;
+using ExpensesManager.Infra.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ListaComprasApi.Infra.CrossCutting.IoC
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+
+            services.AddScoped<IUserReadRepository, UserReadRepository>();
+            services.AddScoped<IUserWriteRepository, UserWriteRepository>();
+            services.AddScoped<IPersonServiceApplication, PersonServiceApplication>();
+            services.AddScoped<IPersonWriteRepository, PersonWriteRepository>();
+            services.AddScoped<IPersonReadRepository, PersonReadRepository>();        
+
+            return services;
+        }
+    }
+}
