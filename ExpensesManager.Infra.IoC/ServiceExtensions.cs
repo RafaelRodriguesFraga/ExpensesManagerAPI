9262315cb1de2c70f13b2532361f735c428b4d9b
Rafael Rodrigues Fraga <ti.rafaelfraga@gmail.com>
2024-04-27 22:54:22 -0300

    Add dependency for service and repository

--- ExpensesManager.Infra.IoC/ServiceExtensions.cs
+++ ExpensesManager.Infra.IoC/ServiceExtensions.cs
@@ -2,6 +2,8 @@
 using ExpensesManager.Application.Services;
 using ExpensesManager.Application.Services.Token;
 using ExpensesManager.Application.Services.User;
+using ExpensesManager.Application.Services.Expense;
+using ExpensesManager.Application.Services.Person;
 
 namespace ExpensesManager.Infra.IoC
 {
@@ -11,6 +13,9 @@ namespace ExpensesManager.Infra.IoC
         {
             services.AddScoped<ITokenServiceApplication, TokenServiceApplication>();
             services.AddScoped<IUserServiceApplication, UserServiceApplication>();
+            services.AddScoped<IPersonServiceApplication, PersonServiceApplication>();
+            services.AddScoped<IExpenseServiceApplication, ExpenseServiceApplication>();
+
 
             return services;
         }
