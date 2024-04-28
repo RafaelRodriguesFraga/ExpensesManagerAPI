e29cc69d0217d1491b03ecc5a41ee75a63cec79c
Rafael Rodrigues Fraga <ti.rafaelfraga@gmail.com>
2024-04-27 22:52:51 -0300

    Fix namespace from ViewModels to ViewModels.Login

--- ExpensesManager.Application/ViewModels/Login/LoginRequestViewModel.cs
+++ ExpensesManager.Application/ViewModels/Login/LoginRequestViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ExpensesManager.Application.Login
+namespace ExpensesManager.Application.ViewModels.Login
 {
     public class LoginRequestViewModel
     {
