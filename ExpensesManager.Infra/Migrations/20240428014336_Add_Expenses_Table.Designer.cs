dc52e681be26a1164d36bb7b555fe169cc341002
Rafael Rodrigues Fraga <ti.rafaelfraga@gmail.com>
2024-04-27 22:54:38 -0300

    Add expenses migration

--- ExpensesManager.Infra/Migrations/20240428014336_Add_Expenses_Table.Designer.cs
+++ ExpensesManager.Infra/Migrations/20240428014336_Add_Expenses_Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ExpensesManager.Infra.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExpensesManager.Infra.Migrations
 {
     [DbContext(typeof(ExpensesManagerContext))]
-    partial class ExpensesManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20240428014336_Add_Expenses_Table")]
+    partial class Add_Expenses_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
